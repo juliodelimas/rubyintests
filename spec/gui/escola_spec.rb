7e9041826f5928ce37f242d86323c79faa96fb72
Júlio de Lima <info@juliodelima.com.br>
2016-07-10 06:10:18 -0300

    Código criado durante a palestra.

--- spec/gui/escola_spec.rb
+++ spec/gui/escola_spec.rb
@@ -1,24 +1,28 @@
 require 'selenium-webdriver'
-describe "Gerenciar Site Escola" do
-	context "Avaliar" do
-		it "alunos com notas maiores ou iguais a 7 são aprovados" do
-			# Instanciando e configurando o navegador web
-			navegador = Selenium::WebDriver.for :firefox
-			navegador.manage.window.maximize()
 
-			# Acessando a página inicial do QuickLoja
-			navegador.get('http://localhost:4567/escola/web/')
-				
-			# Preenchendo o formulário
-			navegador.find_element(:name, 'notaprova').send_keys("7")
-			navegador.find_element(:name, 'notatrabalho').send_keys("7")
-			navegador.find_element(:id, 'avaliar').click()
-			
-			# Validando que entrou
-			expect(navegador.find_element(:id, 'message').text).to eq("Aprovado")
-			
-			# Fechar o browser
-			navegador.close()
-		end
-	end
+describe 'Site Escola' do
+  it 'alunos com notas maiores ou iguais que sete devem ser aprovados' do
+    # 1. Abrir e maximizar o browser Firefox;
+    navegador = Selenium::WebDriver.for :firefox
+    navegador.manage.window.maximize()
+
+    # 2. Acessar a aplicação web;
+    navegador.get('http://localhost:4567/escola/web/')
+
+    # 3. Preencher o formulário;
+    navegador.find_element(:name, 'notaprova').send_keys("7")
+    navegador[:name => 'notatrabalho'].send_keys("7")
+
+    # 4. Clicar no botão “Avaliar”;
+    navegador[:id => "avaliar"].click()
+
+    # 5. Validar que a mensagem “Aprovado" foi apresentada.
+    resultado = navegador[:id => "message"].text
+    expect(resultado).to eq("Aprovado")
+
+    navegador.close()
+
+  end
+  
+  # TODO: Criar testar para validar Exame e DP
 end

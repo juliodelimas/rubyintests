f0036cd60e24b1498840f730a4be6f6adbe62a0d
Júlio de Lima <info@juliodelima.com.br>
2016-07-10 06:05:52 -0300

    Código criado durante a palestra

--- spec/api/escola_spec.rb
+++ spec/api/escola_spec.rb
@@ -1,23 +1,32 @@
 require 'rest-client'
 
-describe "Gerenciar API Escola" do
-	context "/avaliar" do
-		context "GET" do
-			it "alunos com notas maiores ou iguais a 7 são aprovados" do
-				#  Executando a requisição
-				resposta = RestClient.post(
-					'http://localhost:4567/escola/api/avaliar', 
-					:notaprova => '7', 
-					:notatrabalho => '7'
-				)
-				
-				# Convertendo o resultado de JSON para Object
-				corpo    = JSON.parse(resposta.body, object_class: OpenStruct)
-				
-				# Validando resultados
-				expect(corpo.status).to eq("success")
-				expect(corpo.message).to eq("Aprovado")
-			end
-		end
-	end
+describe 'API Escola' do
+  context '/avaliar' do
+    context 'POST' do
+      it 'alunos com notas maiores ou iguais que sete sao aprovados' do
+        # Requisicao ao méTODO
+        resposta = RestClient.post(
+          'http://localhost:4567/escola/api/avaliar',
+          :notaprova => '7',
+          :notatrabalho => '7'
+        )
+
+        # resposta.body
+        # {"status": "success", "message": "Aprovado"}
+        # corpo.message
+
+        # Conversão
+        corpo = JSON.parse(
+          resposta.body,
+          :object_class => OpenStruct
+        )
+
+        # Validações
+        expect(corpo.status).to eq("success")
+        expect(corpo.message).to eq("Aprovado")
+      end
+
+      # TODO: Fazer testes de Exame e DP
+    end
+  end
 end
